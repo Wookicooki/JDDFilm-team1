a5073207fc39980bd3f370485151f2dc48ca10fc
DESKTOP-96DHBRN\it <jmt2066206@gmail.com>
2023-07-03 09:30:55 +0900

    First commmit

---
+++ src/main/java/com/example/jangdocdaefilm/JangDocDaeFilmApplication.java
@@ -0,0 +1,13 @@
+package com.example.jangdocdaefilm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JangDocDaeFilmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JangDocDaeFilmApplication.class, args);
+    }
+
+}
