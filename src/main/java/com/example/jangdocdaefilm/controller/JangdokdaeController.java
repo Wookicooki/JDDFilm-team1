245ddb31a98928c4a0c3d68618e17bde92624904
seaborn0918 <atjhmm@gmail.com>
2023-07-03 13:44:58 +0900

    메인, 영화상세, 리뷰조회, 통합검색

--- src/main/java/com/example/jangdocdaefilm/controller/JangdokdaeController.java
+++ src/main/java/com/example/jangdocdaefilm/controller/JangdokdaeController.java
@@ -1,4 +1,4 @@
-package com.bitc.views.controller;
+package com.example.jangdocdaefilm.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
