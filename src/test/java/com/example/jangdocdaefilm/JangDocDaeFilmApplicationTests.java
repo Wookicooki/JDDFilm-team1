1dd1de049c451afe5232ce89fb80222930d382d4
seaborn0918 <atjhmm@gmail.com>
2023-07-03 13:34:46 +0900

    no message

---
+++ src/test/java/com/example/jangdocdaefilm/JangDocDaeFilmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jangdocdaefilm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JangDocDaeFilmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
--- views/src/main/java/com/bitc/views/controller/JangdokdaeController.java
+++ views/src/main/java/com/bitc/views/controller/JangdokdaeController.java
@@ -1,35 +1,29 @@
 package com.bitc.views.controller;
 
-import lombok.Data;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Controller
 public class JangdokdaeController {
 
-  @RequestMapping(value = "/main", method = RequestMethod.GET)
-  public String mainPage() {
-    return "jangdokdae/main";
-  }
+    @RequestMapping(value = "/main", method = RequestMethod.GET)
+    public String mainPage() {
+        return "jangdokdae/main";
+    }
 
-  @RequestMapping(value = "/movieDetail", method = RequestMethod.GET)
-  public String movieDetailPage() {
-    return "jangdokdae/movieDetail";
-  }
+    @RequestMapping(value = "/movieDetail", method = RequestMethod.GET)
+    public String movieDetailPage() {
+        return "jangdokdae/movieDetail";
+    }
 
-  @RequestMapping(value = "/movieReview", method = RequestMethod.GET)
-  public String movieReviewPage() {
-    return "jangdokdae/movieReview";
-  }
+    @RequestMapping(value = "/movieReview", method = RequestMethod.GET)
+    public String movieReviewPage() {
+        return "jangdokdae/movieReview";
+    }
 
-  @RequestMapping(value = "/search", method = RequestMethod.GET)
-  public String movieSearchPage() {
-    return "jangdokdae/searchResult";
-  }
-
+    @RequestMapping(value = "/search", method = RequestMethod.GET)
+    public String movieSearchPage() {
+        return "jangdokdae/searchResult";
+    }
 }
