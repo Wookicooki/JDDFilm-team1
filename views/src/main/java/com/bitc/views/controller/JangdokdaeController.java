d6aa072fe0e4eea6c0c7531c11e6f31084309050
seaborn0918 <atjhmm@gmail.com>
2023-07-03 10:55:03 +0900

    no message

---
+++ views/src/main/java/com/bitc/views/controller/JangdokdaeController.java
@@ -0,0 +1,29 @@
+package com.bitc.views.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class JangdokdaeController {
+
+    @RequestMapping(value = "/main", method = RequestMethod.GET)
+    public String mainPage() {
+        return "jangdokdae/main";
+    }
+
+    @RequestMapping(value = "/movieDetail", method = RequestMethod.GET)
+    public String movieDetailPage() {
+        return "jangdokdae/movieDetail";
+    }
+
+    @RequestMapping(value = "/movieReview", method = RequestMethod.GET)
+    public String movieReviewPage() {
+        return "jangdokdae/movieReview";
+    }
+
+    @RequestMapping(value = "/search", method = RequestMethod.GET)
+    public String movieSearchPage() {
+        return "jangdokdae/searchResult";
+    }
+}
