da7a714671b3fdc003c5bff48742b994578d7b60
joontae98_mac <jmt2066206@gmail.com>
2023-07-04 16:59:22 +0900

    카테고리 페이지 기능 구현

--- src/main/java/com/example/jangdocdaefilm/controller/MainController.java
+++ src/main/java/com/example/jangdocdaefilm/controller/MainController.java
@@ -5,7 +5,6 @@ import com.example.jangdocdaefilm.service.ParseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -56,11 +55,6 @@ public class MainController {
     @RequestMapping("/signUp")
     public String signUp() throws Exception {
         return "login/signUp";
-    }
-
-    @RequestMapping("/category")
-    public String category() throws Exception {
-        return "movie/category";
     }
 
     @RequestMapping("/recommend")
